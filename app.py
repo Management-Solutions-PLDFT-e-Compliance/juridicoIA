267f7eb35abc1ee7e9d3f931c9696b8a1d7f8556
Raul Miranda <raul.oliveira.miran1@managementsolutions.com>
2024-03-25 10:02:39 -0300

    Adionar botões com função de tamanho da resposta

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from openai import OpenAI
 import time
 import sqlite3
+import PyPDF2
 from flask import Flask, request, render_template, redirect, url_for
 from PyPDF2 import PdfReader
 from config import API_KEY
+import spacy
 
 #Inicialização do flask
 app = Flask(__name__)
@@ -165,16 +167,42 @@ def resumo():
                 resposta += chunk.choices[0].delta.content
         return resposta
 
+        if gerar_resposta_consolidada(objetivo_final) != "Vazio":
+            print("Resultado na base de dados:\n")
+            resumo = gerar_resposta_consolidada(pergunta)
+            print(resumo)
+        else:
+            print("Resposta vazia, refazendo processamento...")
+            ######## INICIO
+
+            # Leitura e divisão do PDF em partes
+            partes_pdf = ler_pdf_e_dividir(caminho_pdf)
+
+            #########################DEFINIR A PERGUNTA 1#########################
+            pergunta_int = f"{pergunta}, responda em no máximo{tamanho_respostas_int} palavras. Responda de forma curta e direta'"
+
+            # Extração da resposta combinada do modelo generativo
+            respostas_partes = analise_textos(pergunta_int, partes_pdf)
+
+            # Tokenização do resultado
+            respostas_concatenadas_1 = ''.join(respostas_partes)
+            respostas_tokenizadas_1 = tokenizar_respostas_com_spacy(respostas_concatenadas_1)
+            print(respostas_tokenizadas_1)
+            print("\n\n\n\n\n\n")
+
+            #DEFINIR A PERGUNTA CONSOLIDADA
+            pergunta_sintese = f"{pergunta}: procurar resposta em: {', '.join(respostas_tokenizadas_1)} em no máximo {tamanho_resposta_final} palavras. Indique, em %, qual a confiabilidade da resposta somente em números. Separe o valor da confiabilidade da sintese por |"
+
+            #GERAR RESUMO
+
+            resumo= gerar_resposta_consolidada_processamento(pergunta_sintese)
+            print(resumo)
+
     # Atribuir os valores a outras variáveis
     numero_processo, autor, juiz, reu, advogado_autor, advogado_reu, valor_indenizacao, juizado, comarca, vara, data_abertura, data_atualizacao, status, sintese, acuracia, resumo_partes = resultado
     
     resumo= gerar_resposta_consolidada(pergunta)
 
-    juizQuantidade = "N/A"
-    clienteQuantidade = "N/A"
-    tribunalQuantidade = "N/A"
-    produto = "Empréstimo"
-    tipoReclamacao = "Contratação indevida"
 
     if num_processo == "0052211-83.2020.8.06.0029 ":
 
@@ -218,12 +246,26 @@ def pergunta():
 
     global num_processo
 
+    resumo = ""
     perguntaUser =""
+    tamanhoUser = ""
 
     perguntaUser = request.form['texto']
+    tamanhoUser = request.form['valor']
     
+    print(perguntaUser, tamanhoUser)
+
     pergunta = perguntaUser
-    tamanho_resposta = "300"
+    tamanho_resposta = "60"
+    bases = 'C:/Users/raul.oliveira.miran1/Downloads/database/'
+    arquivo = '61pag.pdf'
+    caminho_pdf = bases + arquivo
+    num_processo = "0022701-03.2020.8.05.0110"
+    tamanho_resposta_final = 10
+    tamanho_respostas_int = tamanho_resposta_final*2
+
+    # Carregar o modelo de língua portuguesa do spaCy
+    nlp = spacy.load("pt_core_news_sm")
 
     # Conectar ao banco de dados
     conn = sqlite3.connect('C:/Users/raul.oliveira.miran1/Downloads/database/dados_processos.db')
@@ -243,11 +285,71 @@ def pergunta():
     # Fechar a conexão
     conn.close()
 
+    # Funções
+    def ler_pdf_e_dividir(caminho_arquivo, max_tokens=3000):
+        leitor_pdf = PyPDF2.PdfReader(caminho_arquivo)
+        partes = []
+        texto_atual = ''
+        tokens_atual = 0
+        for pagina in leitor_pdf.pages:
+            texto_pagina = pagina.extract_text() + "\n"
+            tokens_pagina = len(texto_pagina.split())
+            if tokens_atual + tokens_pagina > max_tokens:
+                partes.append(texto_atual)
+                texto_atual = texto_pagina
+                tokens_atual = tokens_pagina
+            else:
+                texto_atual += texto_pagina
+                tokens_atual += tokens_pagina
+        if texto_atual:
+            partes.append(texto_atual)
+        return partes
+
+    def analise_textos(objetive, partes):
+        max_tokens_input = 3000
+        max_tokens_output = 900
+        resposta_final = ''
+        for i, parte in enumerate(partes):  # Adiciona um loop para iterar sobre as partes e um contador (i) para acompanhar o número da parte
+            print(f"Analisando parte {i+1} de {len(partes)}")  # Imprime o número da parte atual
+            # Divide o texto em partes menores de max_tokens_input caracteres (para deixar uma margem de segurança)
+            partes_texto = [parte[i:i+max_tokens_input] for i in range(0, len(parte), max_tokens_input)]
+            for parte_texto in partes_texto:
+                stream = client.chat.completions.create(
+                    model="gpt-4",
+                    messages=[{"role": "user", "content": f"{objetive} \n\n{parte_texto}"}],
+                    stream=True,
+                    max_tokens=max_tokens_output  # Define o max_tokens_output para a chamada da API
+                )
+                for chunk in stream:
+                    if chunk.choices[0].delta.content is not None:
+                        resposta_final += chunk.choices[0].delta.content
+        return resposta_final
+
+
+    def tokenizar_respostas_com_spacy(respostas):
+        doc = nlp(respostas)  # Aqui, 'respostas' deve ser uma string única contendo todo o texto.
+        todas_sentencas = [sent.text.strip() for sent in doc.sents]
+        return todas_sentencas
+
+    def gerar_resposta_consolidada_processamento(objetivo_final):
+        resposta = ''
+        stream = client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": f"{objetivo_final}"}],
+            stream=True,
+        )
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                resposta += chunk.choices[0].delta.content
+        return resposta
+
+
+
     def gerar_resposta_consolidada(objetivo_final):
         resposta = ''
         stream = client.chat.completions.create(
             model="gpt-4",
-            messages=[{"role": "user", "content": f" Imprima o resultado em HTML, {objetivo_final}, considerando \n\n {resumo_partes} em no máximo {tamanho_resposta}."}],
+            messages=[{"role": "user", "content":f"{objetivo_final}, considerando \n\n {resumo_partes} em no máximo {tamanho_resposta}. Se não for possível encontrar a resposta, retorne a palavra 'Vazio'"}],
             stream=True,
         )
         for chunk in stream:
@@ -257,58 +359,109 @@ def pergunta():
 
     # Atribuir os valores a outras variáveis
     numero_processo, autor, juiz, reu, advogado_autor, advogado_reu, valor_indenizacao, juizado, comarca, vara, data_abertura, data_atualizacao, status, sintese, acuracia, resumo_partes = resultado
+
+    if gerar_resposta_consolidada(pergunta) != "Vazio":
+        print("Resultado na base de dados:\n")
+        resumo = gerar_resposta_consolidada(pergunta)
+        print(resumo)
+    else:
+        print("Resposta vazia, refazendo processamento...")
+        ######## INICIO
+
+        # Leitura e divisão do PDF em partes
+        partes_pdf = ler_pdf_e_dividir(caminho_pdf)
+
+        #########################DEFINIR A PERGUNTA 1#########################
+        pergunta_int = f"{pergunta}, responda em no máximo{tamanho_respostas_int} palavras. Responda de forma curta e direta'"
+
+        # Extração da resposta combinada do modelo generativo
+        respostas_partes = analise_textos(pergunta_int, partes_pdf)
+
+        # Tokenização do resultado
+        respostas_concatenadas_1 = ''.join(respostas_partes)
+        respostas_tokenizadas_1 = tokenizar_respostas_com_spacy(respostas_concatenadas_1)
+        print(respostas_tokenizadas_1)
+        print("\n\n\n\n\n\n")
+
+        #DEFINIR A PERGUNTA CONSOLIDADA
+        pergunta_sintese = f"{pergunta}: procurar resposta em: {', '.join(respostas_tokenizadas_1)} em no máximo {tamanho_resposta_final} palavras."
+
+        #GERAR RESUMO
+
+        resumo= gerar_resposta_consolidada_processamento(pergunta_sintese)
+        print(resumo)
     
-    resumo= gerar_resposta_consolidada(pergunta)
+        print("\n\nResumo:"+resumo)
 
-    print(pergunta)
-    print(resumo)
+        juizQuantidade = "N/A"
+        clienteQuantidade = "N/A"
+        tribunalQuantidade = "N/A"
+        produto = "Empréstimo"
+        tipoReclamacao = "Contratação indevida"
 
+        if num_processo == "0052211-83.2020.8.06.0029 ":
+
+            juiz = "Karla Cristina de Oliveira"
+            autor = "FRANCISCA ROMANA BEZERRA DA COSTA"
+            reu = "BANCO BRADESCO S.A"
+            advogado_autor = "Rangel Pereira Ribeiro"
+            advogado_reu = "FRANCISCO SAMPAIO DE MENEZES JUNIOR"
+            valor_indenizacao = "23.054,24"
+            juizado = "TJCE"
+            comarca = "TJCE"
+            data_abertura = "14/12/2020"
+            data_atualizacao = "10/11/2021"
+            juizQuantidade = "N/A"
+            clienteQuantidade = "N/A"
+            tribunalQuantidade = "N/A"
+            produto = "Empréstimo"
+            tipoReclamacao = "Contratação indevida"
+                
+        elif num_processo == "0022701-03.2020.8.05.0110":
+            juiz = "Alexandre Lopes"
+            autor = "Delci Rodrigues Santana Silva"
+            reu = "Banco Bradesco Financiamento S A"
+            advogado_autor = "Davi Olinto Soares"
+            advogado_reu = "Fernando Augusto de Faria Corbo"
+            valor_indenizacao = "15.000,00"
+            juizado = "TJBA"
+            comarca = "TJBA"
+            data_abertura = "03/12/2020"
+            data_atualizacao = "28/07/2023"
+            juizQuantidade = "N/A"
+            clienteQuantidade = "N/A"
+            tribunalQuantidade = "N/A"
+            produto = "Empréstimo"
+            tipoReclamacao = "Contratação indevida"
+        else:  
+            juiz = "BERNARDO MARIO DANTAS LUBAMBO"
+            autor = "JORGE PEREIRA DA SILVA"
+            reu = "BANCO BRADESCO S.A"
+            advogado_autor = "EMILIO LOPES DA CRUZ"
+            advogado_reu = "FERNANDO AUGUSTO DE FARIA CORBO"
+            valor_indenizacao = "9.583,04"
+            juizado = "TJBA"
+            comarca = "TJBA"
+            data_abertura = "12/04/2021"
+            data_atualizacao = "18/04/2023"
+            juizQuantidade = "N/A"
+            clienteQuantidade = "N/A"
+            tribunalQuantidade = "N/A"
+            produto = "Empréstimo"
+            tipoReclamacao = "Contratação indevida"
+    
     juizQuantidade = "N/A"
     clienteQuantidade = "N/A"
     tribunalQuantidade = "N/A"
     produto = "Empréstimo"
     tipoReclamacao = "Contratação indevida"
 
-    if num_processo == "0052211-83.2020.8.06.0029 ":
-
-        juiz = "Karla Cristina de Oliveira"
-        autor = "FRANCISCA ROMANA BEZERRA DA COSTA"
-        reu = "BANCO BRADESCO S.A"
-        advogado_autor = "Rangel Pereira Ribeiro"
-        advogado_reu = "FRANCISCO SAMPAIO DE MENEZES JUNIOR"
-        valor_indenizacao = "23.054,24"
-        juizado = "TJCE"
-        comarca = "TJCE"
-        data_abertura = "14/12/2020"
-        data_atualizacao = "10/11/2021"
-    elif num_processo == "0022701-03.2020.8.05.0110":
-        juiz = "Alexandre Lopes"
-        autor = "Delci Rodrigues Santana Silva"
-        reu = "Banco Bradesco Financiamento S A"
-        advogado_autor = "Davi Olinto Soares"
-        advogado_reu = "Fernando Augusto de Faria Corbo"
-        valor_indenizacao = "15.000,00"
-        juizado = "TJBA"
-        comarca = "TJBA"
-        data_abertura = "03/12/2020"
-        data_atualizacao = "28/07/2023"
-    else:  
-        juiz = "BERNARDO MARIO DANTAS LUBAMBO"
-        autor = "JORGE PEREIRA DA SILVA"
-        reu = "BANCO BRADESCO S.A"
-        advogado_autor = "EMILIO LOPES DA CRUZ"
-        advogado_reu = "FERNANDO AUGUSTO DE FARIA CORBO"
-        valor_indenizacao = "9.583,04"
-        juizado = "TJBA"
-        comarca = "TJBA"
-        data_abertura = "12/04/2021"
-        data_atualizacao = "18/04/2023"
-
     return render_template('index.html', numero_processo=numero_processo, autor=autor, reu=reu, advogado_autor=advogado_autor, advogado_reu=advogado_reu, data_abertura=data_abertura, data_atualizacao=data_atualizacao, acuracia=acuracia, status=status, valor_indenizacao=valor_indenizacao, vara=vara, comarca=comarca, juizado=juizado, juiz=juiz, juizQuantidade=juizQuantidade, clienteQuantidade=clienteQuantidade, tribunalQuantidade=tribunalQuantidade, resumo_partes=resumo_partes, produto=produto, tipoReclamacao=tipoReclamacao, resumo=resumo, pergunta=pergunta)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 inicio_resumo = time.time()
 
-resumo= gerar_resposta_consolidada(pergunta)
-print(resumo)
+#resumo= gerar_resposta_consolidada(pergunta)
+#print(resumo)
--- static/script.js
+++ static/script.js
@@ -39,3 +39,61 @@ function toggleDislike() {
     likeImg.src = "/static/assets/like.png";
 }
 
+
+function perguntar() {
+  var pergunta = document.getElementById("texto").value;
+  var valor = document.getElementById("valor").value;
+  fetch('/pergunta', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({ pergunta: pergunta, valor: valor }),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      alert("Pergunta enviada com sucesso!");
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+      alert("Ocorreu um erro ao enviar a pergunta.");
+  });
+}
+
+document.getElementById("respostaCurta").addEventListener("click", function() {
+  document.getElementById("valor").value = 100;
+});
+
+document.getElementById("respostaMedia").addEventListener("click", function() {
+  document.getElementById("valor").value = 250;
+});
+
+document.getElementById("respostaLonga").addEventListener("click", function() {
+  document.getElementById("valor").value = 500;
+});
+
+// Seleciona o botão padrão (Média) e remove a classe btn-sintese-active
+document.getElementById('respostaMedia').classList.remove('btn-sintese-active');
+
+// Adiciona um evento de clique para cada botão
+document.getElementById('respostaCurta').addEventListener('click', function() {
+  // Remove a classe btn-sintese-active de todos os botões
+  document.getElementById('respostaCurta').classList.add('btn-sintese-active');
+  document.getElementById('respostaMedia').classList.remove('btn-sintese-active');
+  document.getElementById('respostaLonga').classList.remove('btn-sintese-active');
+});
+
+document.getElementById('respostaMedia').addEventListener('click', function() {
+  // Remove a classe btn-sintese-active de todos os botões
+  document.getElementById('respostaCurta').classList.remove('btn-sintese-active');
+  document.getElementById('respostaMedia').classList.add('btn-sintese-active');
+  document.getElementById('respostaLonga').classList.remove('btn-sintese-active');
+});
+
+document.getElementById('respostaLonga').addEventListener('click', function() {
+  // Remove a classe btn-sintese-active de todos os botões
+  document.getElementById('respostaCurta').classList.remove('btn-sintese-active');
+  document.getElementById('respostaMedia').classList.remove('btn-sintese-active');
+  document.getElementById('respostaLonga').classList.add('btn-sintese-active');
+});
